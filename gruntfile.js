37a0f8d8ad28f6f866c59a4e8767a028120d6cd4
Beto Costa <robertomarx@gmail.com>
2018-11-07 17:49:56 -0200

    grunt part 2

--- gruntfile.js
+++ gruntfile.js
@@ -2,7 +2,9 @@
 
 module.exports = function (grunt) {
     require('time-grunt')(grunt);
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
 
     grunt.initConfig({
         sass: {
@@ -35,6 +37,115 @@ module.exports = function (grunt) {
                     browser: ['chrome']
                 }
             }
+        },
+        copy: {
+            html: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        clean: {
+            build: {
+                src: ['dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['img/*.{png,jpg,gif}'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html, aboutus.html, index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block) {
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {}
+        },
+        uglify: {
+            dist: {}
+        },
+        cssmin: {
+            dist: {}
+        },
+        // filerev: file revision number:
+        // Avoids problems with cached old files. Will make the user browser download new modified files.
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+            release: {
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css'
+                    ]
+                }]
+            }
+        },
+        usemin: {
+            html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/index.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css', 'dist/js']
+            }
+        },
+        // HTMLmin needs to run after use min, that why it's in this order.
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html',
+                    'dist/contactus.html': 'dist/contactus.html',
+                    'dist/aboutus.html': 'dist/aboutus.html'
+                }
+            }
         }
     });
     // To make grunt register and run the task. Probably not needed since sass is used with
@@ -42,7 +153,22 @@ module.exports = function (grunt) {
     grunt.registerTask('css',['sass']);
     // Need to be in this order, browserSync first and watch later, otherwise, watch would
     // close the other tasks and browserSync would not run.
-    grunt.registerTask('default',['browserSync', 'watch']);
     // then use 'grunt css' on the console to run this.
+
+    // Using the tasks below, we can just use 'grunt' to run the default one:
+    grunt.registerTask('default',['browserSync', 'watch']);
+    // Using 'grunt build' to run the task below:
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ]);
 
 };
