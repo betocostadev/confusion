cd95bc7f5ab2831f733d581f675595a5da9a315a
Beto Costa <robertomarx@gmail.com>
2018-11-08 15:57:42 -0200

    gulp part 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCSS = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
 gulp.task('sass', function() {
     return gulp.src('./css/*.scss')
@@ -30,6 +38,48 @@ gulp.task('browserSync', function() {
     });
 });
 
+// Default task
 gulp.task('default', ['browserSync'], function() {
     gulp.start('sass:watch');
+});
+
+// Adding the del (delete dist folder) plugin
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+// Copy the fonts into 'dist' folder
+gulp.task('copyfonts', function() {
+    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+    .pipe(gulp.dest('./dist/fonts'));
+});
+
+// Imagemin
+gulp.task('imagemin', function() {
+    return gulp.src('img/*.{png,jpg,gif}')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/img'));
+});
+
+// Usemin task takes the html files and looks for the css and js to concat them on the dist folder.
+// Flatmap pipe proccess each html file to search for its dependencies
+// Rev will add the code to the end of the files, in the case below, the css and js files.
+gulp.task('usemin', function() {
+    return gulp.src('./*.html')
+    .pipe(flatmap(function(stream, file){
+        return stream
+        .pipe(usemin({
+            css: [rev()],
+            html: [ function() { return htmlmin({ collapseWhitespace: true })}],
+            js: [ uglify(), rev()],
+            inlinejs: [ uglify()],
+            inlinecss: [ cleanCSS(), 'concat']
+        }))
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+// Build task - ['clean'] task is in this order so it is executed first.
+gulp.task('build', ['clean'], function() {
+    gulp.start('copyfonts', 'imagemin', 'usemin');
 });
